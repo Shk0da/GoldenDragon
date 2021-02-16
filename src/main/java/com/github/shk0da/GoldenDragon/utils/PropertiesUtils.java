fffc177d3a265477d24ddaf2576d222ca10d5394
Anatoly.Shkondin <anatoly.shkondin@nordigy.ru>
2021-02-16 12:05:48 +0300

    add external properties

--- src/main/java/com/github/shk0da/GoldenDragon/utils/PropertiesUtils.java
+++ src/main/java/com/github/shk0da/GoldenDragon/utils/PropertiesUtils.java
@@ -1,16 +1,28 @@
 package com.github.shk0da.GoldenDragon.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
 import java.util.Properties;
 
 public final class PropertiesUtils {
 
     public static Properties loadProperties() throws IOException {
-        InputStream inputStream = PropertiesUtils.class.getClassLoader().getResourceAsStream("application.properties");
         final Properties properties = new Properties();
-        properties.load(Objects.requireNonNull(inputStream));
+
+        InputStream internalProperties = PropertiesUtils.class.getClassLoader().getResourceAsStream("application.properties");
+        properties.load(Objects.requireNonNull(internalProperties));
+
+        String externalPathProperties = System.getProperty("application.properties");
+        if (null != externalPathProperties && Files.exists(Path.of(externalPathProperties))) {
+            InputStream externalProperties = new FileInputStream(new File(externalPathProperties));
+            properties.load(Objects.requireNonNull(externalProperties));
+        }
+
         return properties;
     }
 }
