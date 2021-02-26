8510293202d4f1f181c994d13106c2455f35a002
Anatoly.Shkondin <anatoly.shkondin@nordigy.ru>
2021-02-26 13:02:52 +0300

    MarketConfig from application.properties

--- src/main/java/com/github/shk0da/GoldenDragon/config/DivFlowConfig.java
+++ src/main/java/com/github/shk0da/GoldenDragon/config/DivFlowConfig.java
@@ -1,12 +1,7 @@
 package com.github.shk0da.GoldenDragon.config;
 
 public class DivFlowConfig {
-
     public static final String SMART_LAB_DIV_CALENDAR = "https://smart-lab.ru/dividends/index/order_by_cut_off_date/desc/?is_approved=1";
     public static final String DOHOD_DIV_CALENDAR = "https://www.dohod.ru/ik/analytics/dividend";
     public static final String INVESTING_DIV_CALENDAR = "https://ru.investing.com/dividends-calendar/Service/getCalendarFilteredData";
-
-    public static final String SCAN_US = "https://scanner.tradingview.com/america/scan";
-    public static final String SCAN_MOEX = "https://scanner.tradingview.com/russia/scan";
-
 }
--- src/main/java/com/github/shk0da/GoldenDragon/config/MarketConfig.java
+++ src/main/java/com/github/shk0da/GoldenDragon/config/MarketConfig.java
@@ -1,8 +1,17 @@
 package com.github.shk0da.GoldenDragon.config;
 
 import com.github.shk0da.GoldenDragon.model.Market;
+import com.github.shk0da.GoldenDragon.utils.PropertiesUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static java.lang.System.out;
 
 public final class MarketConfig {
+
+    private static final Map<Market, MarketConfig> defaultMarketConfigs = new HashMap<>();
 
     private final Market market;
     private final int startWorkHour;
@@ -10,7 +19,35 @@ public final class MarketConfig {
     private final int maxPositionCostToBuy;
     private final String currency;
 
-    public MarketConfig(Market market, int startWorkHour, int endWorkHour, int maxPositionCostToBuy, String currency) {
+    static {
+        try {
+            final Properties properties = PropertiesUtils.loadProperties();
+            defaultMarketConfigs.put(
+                    Market.US,
+                    new MarketConfig(
+                            Market.US,
+                            Integer.parseInt(properties.getProperty("market.us.startWorkHour", "16")),
+                            Integer.parseInt(properties.getProperty("market.us.endWorkHour", "19")),
+                            Integer.parseInt(properties.getProperty("market.us.maxPositionCostToBuy", "1000")),
+                            properties.getProperty("market.us.currency", "USD"))
+                    );
+            defaultMarketConfigs.put(
+                    Market.MOEX,
+                    new MarketConfig(
+                            Market.MOEX,
+                            Integer.parseInt(properties.getProperty("market.moex.startWorkHour", "10")),
+                            Integer.parseInt(properties.getProperty("market.moex.endWorkHour", "18")),
+                            Integer.parseInt(properties.getProperty("market.moex.maxPositionCostToBuy", "100000")),
+                            properties.getProperty("market.moex.currency", "RUB"))
+            );
+        } catch (Exception ex) {
+            out.println("Failed load Market config: " + ex.getMessage());
+            defaultMarketConfigs.put(Market.US, createDefaultUS());
+            defaultMarketConfigs.put(Market.MOEX, createDefaultMOEX());
+        }
+    }
+
+    private MarketConfig(Market market, int startWorkHour, int endWorkHour, int maxPositionCostToBuy, String currency) {
         this.market = market;
         this.startWorkHour = startWorkHour;
         this.endWorkHour = endWorkHour;
@@ -19,21 +56,14 @@ public final class MarketConfig {
     }
 
     public static MarketConfig byMarket(Market market) {
-        MarketConfig marketConfig;
-        switch (market) {
-            case US:
-                marketConfig = new MarketConfig(Market.US, 16, 19, 1_000, "USD");
-                break;
-            case MOEX:
-                marketConfig = new MarketConfig(Market.MOEX, 10, 18, 100_000, "RUB");
-                break;
-            default:
-                marketConfig = MarketConfig.createDefault();
-        }
-        return marketConfig;
+        return defaultMarketConfigs.getOrDefault(market, MarketConfig.createDefaultMOEX());
     }
 
-    public static MarketConfig createDefault() {
+    private static MarketConfig createDefaultUS() {
+        return new MarketConfig(Market.US, 16, 19, 1_000, "USD");
+    }
+
+    private static MarketConfig createDefaultMOEX() {
         return new MarketConfig(Market.MOEX, 10, 18, 100_000, "RUB");
     }
 
