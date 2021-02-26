e66d8f41530894fd432b85190719ce96c8e163d0
Anatoly.Shkondin <anatoly.shkondin@nordigy.ru>
2021-02-26 13:06:43 +0300

    Update RSXConfig

--- src/main/java/com/github/shk0da/GoldenDragon/config/RSXConfig.java
+++ src/main/java/com/github/shk0da/GoldenDragon/config/RSXConfig.java
@@ -8,19 +8,13 @@ public class RSXConfig {
 
     public static final String SERIALIZE_NAME = "rsx.json";
 
-    private final String spy;
     private final String trendStock;
     private final int stockPortfolioMaxSize;
 
     public RSXConfig() throws Exception {
         final Properties properties = PropertiesUtils.loadProperties();
-        spy = properties.getProperty("rsx.spy", "SPY");
         trendStock = properties.getProperty("rsx.trendStock", "IWM");
         stockPortfolioMaxSize = Integer.parseInt(properties.getProperty("rsx.stockPortfolioMaxSize", "5"));
-    }
-
-    public String getSpy() {
-        return spy;
     }
 
     public String getTrendStock() {
