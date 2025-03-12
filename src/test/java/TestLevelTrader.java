e115a03bad7105f26b1588212e14c50fe57f0ecd
a.shkondin <a.shkondin@tinkoff.ru>
2025-03-12 22:45:02 +0300

    Tests

--- src/test/java/TestLevelTrader.java
+++ src/test/java/TestLevelTrader.java
@@ -30,7 +30,6 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
