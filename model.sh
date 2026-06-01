c2198dc7be0a87b9ec47a7c3f0a93b98ff521b97
a.shkondin <a.shkondin@tbank.ru>
2026-06-01 09:27:06 +0300

    Update new Strategies

--- model.sh
+++ model.sh
@@ -2,4 +2,4 @@
 cd /home/apps/dragon/ &&
 mkdir -p "logs"
 date=$(date '+%Y-%m-%d')
-java -Dapplication.properties=./application.properties -jar GoldenDragon-1.0.jar GenerateModel --data=ml_strategy/data_pipeline/trades.csv --output=models/trade_classifier_v2.txt --report-dir=ml_strategy > logs/"${date}_GenerateModel.log"
+java -Dapplication.properties=./application.properties -jar GoldenDragon-1.0.jar GenerateModel > logs/"${date}_GenerateModel.log"
