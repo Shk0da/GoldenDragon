e6c104d604291edfa1bb75fd285da72e3f5e4e9d
a.shkondin <a.shkondin@tbank.ru>
2026-07-15 21:37:07 +0300

    Update UnifiedStrategy

--- model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/apps/dragon/ &&
-mkdir -p "logs"
-date=$(date '+%Y-%m-%d')
-java -Dapplication.properties=./application.properties -jar GoldenDragon-1.0.jar GenerateModel > logs/"${date}_GenerateModel.log"
